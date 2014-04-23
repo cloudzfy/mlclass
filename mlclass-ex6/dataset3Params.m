12b98ee72553dd27b91d39b5655cbd3742db9279
cloudzfy <cloudzfy@gmail.com>
2014-04-23 16:50:02 +0800

    Support Vector Machines

---
+++ mlclass-ex6/dataset3Params.m
@@ -0,0 +1,41 @@
+function [C, sigma] = dataset3Params(X, y, Xval, yval)
+%EX6PARAMS returns your choice of C and sigma for Part 3 of the exercise
+%where you select the optimal (C, sigma) learning parameters to use for SVM
+%with RBF kernel
+%   [C, sigma] = EX6PARAMS(X, y, Xval, yval) returns your choice of C and 
+%   sigma. You should complete this function to return the optimal C and 
+%   sigma based on a cross-validation set.
+%
+
+% You need to return the following variables correctly.
+C = 1;
+sigma = 0.3;
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Fill in this function to return the optimal C and sigma
+%               learning parameters found using the cross validation set.
+%               You can use svmPredict to predict the labels on the cross
+%               validation set. For example, 
+%                   predictions = svmPredict(model, Xval);
+%               will return the predictions on the cross validation set.
+%
+%  Note: You can compute the prediction error using 
+%        mean(double(predictions ~= yval))
+%
+
+CVec = [0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30];
+sigmaVec = [0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30];
+predictions = zeros(8,8);
+for i = 1:8
+    for j = 1:8
+        model= svmTrain(X, y, CVec(i), @(x1, x2) gaussianKernel(x1, x2, sigmaVec(j)));
+        predictions(i,j) = mean(double(svmPredict(model, Xval) ~= yval));
+    end
+end
+[i,j] = find(predictions == min(min(predictions)));
+C = CVec(i);
+sigma = sigmaVec(j);
+
+% =========================================================================
+
+end
